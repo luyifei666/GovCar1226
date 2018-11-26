26c942c82a8b92109b15d730bc912f7b2e61c028
bboluyifei@163.com <bboluyifei@163.com>
2018-11-26 15:34:20 +0800

    做了网图加载器

--- GovCar/app/src/main/java/com/clfsjkj/govcar/ItemAdapter/EnclosureAdapter.java
+++ GovCar/app/src/main/java/com/clfsjkj/govcar/ItemAdapter/EnclosureAdapter.java
@@ -1,19 +1,14 @@
 package com.clfsjkj.govcar.ItemAdapter;
 
-import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.clfsjkj.govcar.MainApplication;
 import com.clfsjkj.govcar.R;
 import com.clfsjkj.govcar.bean.EnclosureBean;
 import com.kevin.photo_browse.main.GlideApp;
-
-import java.io.File;
 import java.util.List;
 
 public class EnclosureAdapter extends BaseQuickAdapter<EnclosureBean, BaseViewHolder> {
@@ -24,18 +19,10 @@ public class EnclosureAdapter extends BaseQuickAdapter<EnclosureBean, BaseViewHo
 
     @Override
     protected void convert(BaseViewHolder helper, EnclosureBean item) {
-//        helper.setText(R.id.text, item.getTitle());
-//        helper.setImageResource(R.id.icon, item.getImageResource());
         mContext = MainApplication.getContext();
-//        GlideApp.with(mContext).
-//                load(item.getPicUrl())
-////                .crossFade()
-////                .error(R.mipmap.default_image)//加载错误的替换图
-////                .diskCacheStrategy(DiskCacheStrategy.ALL)//缓存最终图片
-//                .into((ImageView) helper.getView(R.id.iv_img));
-
         GlideApp.with(mContext)
                 .load(item.getPicUrl())
+                .thumbnail(0.1f)
                 .placeholder(com.kevin.photo_browse.R.drawable.img_placeholder)
                 .error(com.kevin.photo_browse.R.drawable.img_error)
                 .into((ImageView) helper.getView(R.id.iv_img));
